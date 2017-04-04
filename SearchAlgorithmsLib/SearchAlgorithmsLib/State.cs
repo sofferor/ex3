efda69c69c8dc770aea7997428324ac5ae84126a
or <sofferor>
2017-04-04 18:18:23 +0300

    df

---
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/State.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithmsLib {
+    public class State<T> {
+        private T state; // the state represented by a string
+        private double cost;
+        private State<T> cameFrom;
+
+        publicState(T state) { }
+
+    }
+}
