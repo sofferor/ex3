3bc82d87972845cc189ac5a84df4fc93df28aa72
or <sofferor>
2017-04-04 20:16:54 +0300

    GENERIC

--- SearchAlgorithmsLib/SearchAlgorithmsLib/ISearchable.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/ISearchable.cs
@@ -1,5 +1,11 @@
-﻿namespace SearchAlgorithmsLib {
-    public interface ISearchable {
-        
+﻿using System.Collections.Generic;
+
+namespace SearchAlgorithmsLib {
+    public interface ISearchable<T> {
+        State<T> getInitialState();
+
+        State<T> getGoalState();
+
+        List<State<T>> getAllPossibleStates(State<T> s);
     }
 }
---
+++ searchalgorithmslib/searchalgorithmslib/searcher.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Priority_Queue;
+
+namespace SearchAlgorithmsLib
+{
+    public abstract class Searcher<T> : ISearcher<T> {
+
+        private SimplePriorityQueue<State<T>> pQueue;
+        private int evaluateNodes;
+
+        //property size of queue.
+        public int QueueSize {
+            get {
+                return pQueue.Count;
+            }
+        }
+
+        public Searcher() {
+            pQueue = new SimplePriorityQueue<State<T>>();
+            evaluateNodes = 0;
+        }
+
+        protected State<T> PopPQueue() {
+            evaluateNodes++;
+            return pQueue.Dequeue();
+        }
+
+        Solution Search(ISearchable<T> searchable) {
+            return new Solution();
+        }
+
+        public int GetNumberOfNodesEvaluated() {
+            return evaluateNodes;
+        }
+    }
+}
