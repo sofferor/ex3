cab880608f88b4bc77b1873cb31660d01cd78507
Haim Weitman <weithaim@gmail.com>
2017-04-04 18:30:37 +0300

    interface ISearchable

--- SearchAlgorithmsLib/SearchAlgorithmsLib/ISearchable.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/ISearchable.cs
@@ -1,5 +1,10 @@
-﻿namespace SearchAlgorithmsLib {
+﻿using System.Collections.Generic;
+
+namespace SearchAlgorithmsLib {
     public interface ISearchable {
-        
+        State<T> GetInitialState<T>();
+        State<T> GetGoalState<T>();
+        List<State<T>> GetAllPossibleStates<T>(State<T> s);
+
     }
 }
