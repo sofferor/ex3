5f2eed1e4fff934e4f11b523f9ff75ec5bc2781d
Haim Weitman <weithaim@gmail.com>
2017-04-04 21:10:24 +0300

    Searchable

--- SearchAlgorithmsLib/SearchAlgorithmsLib/Searchable.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/Searchable.cs
@@ -1,17 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace SearchAlgorithmsLib {
     public abstract class Searchable<T> : ISearchable<T> {
         public State<T> getInitialState() {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public State<T> getGoalState() {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
 
         public List<State<T>> getAllPossibleStates(State<T> s) {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
         }
     }
 }
