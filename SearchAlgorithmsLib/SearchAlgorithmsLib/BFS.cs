e4f2a7fe67dca8934e71541a2fb298531d7d11d5
Haim Weitman <weithaim@gmail.com>
2017-04-04 22:50:28 +0300

    up

--- SearchAlgorithmsLib/SearchAlgorithmsLib/BFS.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/BFS.cs
@@ -1,5 +1,43 @@
-﻿namespace SearchAlgorithmsLib {
-    public class BFS : Searcher<string> {
-        
+﻿using System.Collections.Generic;
+
+namespace SearchAlgorithmsLib {
+    public class BFS<T> : Searcher<T> {
+        public override Solution Search(ISearchable<T> searchable) {
+            PushPQueue(searchable.getInitialState());
+            HashSet<State<T>> closed = new HashSet<State<T>>();
+            int moveCost = 1;
+
+            while (QueueSize > 0) {
+                State<T> n = PopPQueue();
+                closed.Add(n);
+                if (n.Equals(searchable.getGoalState())) {
+                    return BackTrace();
+                }
+
+                List<State<T>> succerssors = searchable.getAllPossibleStates(n);
+                foreach (State<T> s in succerssors) {
+                    if (closed.Contains(s)) {
+                        continue;
+                    }
+
+                    if (!PQcontain(s)) {
+                        s.CameFrom = n;
+                        PushPQueue(s);
+                    } else {
+                        if (s.Cost > n.Cost + moveCost) {
+                            s.CameFrom = n;
+                            s.Cost = n.Cost + moveCost;
+                            UpdatePriority(s);
+                        }
+                    }
+                }
+            }
+
+            
+        }
+
+        private Solution BackTrace() {
+            return new Solution();
+        }
     }
 }
