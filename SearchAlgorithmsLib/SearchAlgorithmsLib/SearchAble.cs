12a58682ef0132904392862d989421b635cf583a
Haim Weitman <weithaim@gmail.com>
2017-04-04 21:10:43 +0300

    s

--- SearchAlgorithmsLib/SearchAlgorithmsLib/SearchAble.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/SearchAble.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace SearchAlgorithmsLib {
     public abstract class Searchable<T> : ISearchable<T> {
+        public State<T> getInitialState() {
+            throw new NotImplementedException();
+        }
 
+        public State<T> getGoalState() {
+            throw new NotImplementedException();
+        }
+
+        public List<State<T>> getAllPossibleStates(State<T> s) {
+            throw new NotImplementedException();
+        }
     }
 }
