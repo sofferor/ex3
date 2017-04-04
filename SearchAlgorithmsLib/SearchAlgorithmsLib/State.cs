905c800a765862bcefeb7c13d05f59bd57162433
Haim Weitman <weithaim@gmail.com>
2017-04-04 18:17:14 +0300

    State<T> class

---
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/State.cs
@@ -0,0 +1,16 @@
+﻿namespace SearchAlgorithmsLib {
+    public class State<T> {
+        private T state;
+        private double cost;
+        private State<T> cameFrom;
+
+        public State(T state) {
+            this.state = state;
+        }
+
+        public bool Equals(State<T> s) {
+            return state.Equals(s.state);
+        }
+
+    }
+}
