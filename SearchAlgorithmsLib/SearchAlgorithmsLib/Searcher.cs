acf5926efb8f20324143a1129e17694be39e1d8f
Haim Weitman <weithaim@gmail.com>
2017-04-04 20:19:12 +0300

    ddd

--- SearchAlgorithmsLib/SearchAlgorithmsLib/Searcher.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/Searcher.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Priority_Queue;
 
 namespace SearchAlgorithmsLib
 {
     public abstract class Searcher : ISearcher {
-        
+        private SimplePriorityQueue<State<string>> pQueue;
+        private int evaluatedNodes;
+
+        public Searcher() {
+            pQueue = new SimplePriorityQueue<State<string>>();
+            this.evaluatedNodes = 0;
+        }
+
+        public abstract Solution Search(ISearchable searchable);
+
+        public int GetNumberOfNodesEvaluated() {
+            return evaluatedNodes;
+        }
+
+        public State<string> PopPQueue() {
+            evaluatedNodes++;
+            State<T> temp = pQueue.First;
+            return ;
+        }
     }
 }
