1357c89188913d1dce68560bf936ebd559caafa9
Haim Weitman <weithaim@gmail.com>
2017-04-05 23:32:03 +0300

    DFS

--- SearchAlgorithmsLib/SearchAlgorithmsLib/DFS.cs
+++ SearchAlgorithmsLib/SearchAlgorithmsLib/DFS.cs
@@ -1,37 +1,33 @@
 ﻿using System.Collections.Generic;
 
 namespace SearchAlgorithmsLib {
-    public class BFS<T> : Searcher<T> {
-        public BFS() { }
+    public class DFS<T> : Searcher<T> {
+        public DFS() { }
 
         public override Solution<T> Search(ISearchable<T> searchable) {
             PushPQueue(searchable.getInitialState());
-            HashSet<State<T>> closed = new HashSet<State<T>>();
-            int moveCost = 1;
+            HashSet<State<T>> visited = new HashSet<State<T>>();
+            int moveCost = -1;
 
             while (QueueSize > 0) {
                 State<T> n = PopPQueue();
-                closed.Add(n);
+                visited.Add(n);
                 if (n.Equals(searchable.getGoalState())) {
                     return BackTrace(searchable.getGoalState());
                 }
 
+                if (visited.Contains(n)) {
+                    continue;
+                }
+                visited.Add(n);
                 List<State<T>> succerssors = searchable.getAllPossibleStates(n);
                 foreach (State<T> s in succerssors) {
-                    if (closed.Contains(s)) {
+                    if (visited.Contains(s)) {
                         continue;
                     }
-
-                    if (!PQcontain(s)) {
-                        s.CameFrom = n;
-                        PushPQueue(s);
-                    } else {
-                        if (s.Cost > n.Cost + moveCost) {
-                            s.CameFrom = n;
-                            s.Cost = n.Cost + moveCost;
-                            UpdatePriority(s);
-                        }
-                    }
+                    s.CameFrom = n;
+                    s.Cost = n.Cost + moveCost;
+                    PushPQueue(s);
                 }
             }
             return null;
