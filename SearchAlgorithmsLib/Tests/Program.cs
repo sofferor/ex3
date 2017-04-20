7bd8b185f215397d5e06afc0540bdec4343824b9
or <sofferor>
2017-04-20 20:45:26 +0300

    all

--- SearchAlgorithmsLib/Tests/Program.cs
+++ SearchAlgorithmsLib/Tests/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using MazeGeneratorLib;
+using MazeLib;
+using SearchAlgorithmsLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,8 +10,31 @@ using System.Threading.Tasks;
 namespace Tests {
     class Program {
         static void Main(string[] args) {
-            ISearcher<String> s = new BFS<String>();
-            s.as();
-        }
+
+            //generate the maze.
+            DFSMazeGenerator mazeGenerator = new DFSMazeGenerator();
+            Maze maze = mazeGenerator.Generate(30, 30);
+            //adapt the maze.
+            SearchableMazeAdapter searchableMaze = new SearchableMazeAdapter(maze);
+
+            //printing the maze.
+            Console.Write(searchableMaze.MyMaze.ToString());
+
+            Solution<Position> BFSSolution;
+            Solution<Position> DFSSolution;
+
+            //solving by BFS.
+            ISearcher<Position> searcher = new BFS<Position>();
+            BFSSolution = searcher.Search(searchableMaze);
+            searchableMaze.Clean();
+
+            //solving by DFS.
+            searcher = new DFS<Position>();
+            DFSSolution = searcher.Search(searchableMaze);
+
+            //printing the solutions.
+            Console.WriteLine(BFSSolution.EvaluatedNodes);
+            Console.WriteLine(DFSSolution.EvaluatedNodes);
+        }//
     }
 }
