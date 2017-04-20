da3f17602523fe7b67bebe7f1fe6ad09c5f94ba4
Haim Weitman <weithaim@gmail.com>
2017-04-21 00:10:26 +0300

    repair

--- SearchAlgorithmsLib/Server/IController.cs
+++ SearchAlgorithmsLib/Server/IController.cs
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Server {
-    interface IController {
-    }
-}
-=======
-﻿namespace Server {
     public interface IController {
         
     }
 }
->>>>>>> ef7484bb7cf1b2b861e34dd58714ebb01319ca5b
+
--- SearchAlgorithmsLib/Server/IView.cs
+++ SearchAlgorithmsLib/Server/IView.cs
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Server {
-    interface IView {
-    }
-}
-=======
-﻿namespace Server {
     public interface IView {
         
     }
 }
->>>>>>> ef7484bb7cf1b2b861e34dd58714ebb01319ca5b
