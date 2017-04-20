24a66e3e1f3dc615e45da03932454efec110773a
Haim Weitman <weithaim@gmail.com>
2017-04-21 00:18:37 +0300

    repair2

--- SearchAlgorithmsLib/Server/Commands/GenerateCommand.cs
+++ SearchAlgorithmsLib/Server/Commands/GenerateCommand.cs
@@ -1,5 +1,12 @@
-﻿namespace Server {
-    public class GenerateCommand {
-        
+﻿using System.Net.Sockets;
+
+namespace Server {
+    public class GenerateCommand : ICommand {
+
+        //members
+        private IModel  
+        public string Execute(string[] args, TcpClient client = null) {
+            throw new System.NotImplementedException();
+        }
     }
 }
