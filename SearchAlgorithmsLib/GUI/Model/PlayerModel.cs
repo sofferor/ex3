fabd42a05ee9241ed8b1b5e0363d87b2ca562fe8
Haim Weitman <weithaim@gmail.com>
2017-05-22 21:24:34 +0300

    player model

---
+++ SearchAlgorithmsLib/GUI/Model/PlayerModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Client;
+
+namespace GUI.Model {
+    public abstract class PlayerModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private Connector connector;
+
+        protected PlayerModel() {
+            connector = new Connector();
+            connector.Initialize(Properties.Settings.Default.ServerIP, Properties.Settings.Default.ServerPort);
+        }
+
+        public string MazeName {
+            get { return Properties.Settings.Default.MazeName; }
+            set { Properties.Settings.Default.MazeName = value; }
+        }
+
+        public int Rows {
+            get { return Properties.Settings.Default.MazeRows; }
+            set { Properties.Settings.Default.MazeRows = value; }
+        }
+
+        public int Cols {
+            get { return Properties.Settings.Default.MazeCols; }
+            set { Properties.Settings.Default.MazeCols = value; }
+        }
+
+        public void Send(string message) {
+            connector.Send(message);
+        }
+
+        public string Receive() {
+            return connector.Receive();
+        }
+
+        protected virtual void NotifyPropertyChanged(string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- SearchAlgorithmsLib/GUI/Model/SinglePlayerModel.cs
+++ SearchAlgorithmsLib/GUI/Model/SinglePlayerModel.cs
@@ -7,36 +7,9 @@ using Client;
 
 
 namespace GUI.Model {
-    class SinglePlayerModel {
+    class SinglePlayerModel : PlayerModel {
 
-        private Connector connector;
-
-        public SinglePlayerModel() {
-            connector = new Connector();
-            connector.Initialize(Properties.Settings.Default.ServerIP, Properties.Settings.Default.ServerPort);
-        }
-
-        public string MazeName {
-            get { return Properties.Settings.Default.MazeName; }
-            set { Properties.Settings.Default.MazeName = value; }
-        }
-
-        public int Rows {
-            get { return Properties.Settings.Default.MazeRows; }
-            set { Properties.Settings.Default.MazeRows = value; }
-        }
-
-        public int Cols {
-            get { return Properties.Settings.Default.MazeCols; }
-            set { Properties.Settings.Default.MazeCols = value; }
-        }
-
-        public void Send(string message) {
-            connector.Send(message);
-        }
-
-        public string Receive() {
-            return connector.Receive();
+        public SinglePlayerModel() : base() {
         }
     }
 }
