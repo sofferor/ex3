f9a255c32b8f3ac2c5871fce981b971fa2c2bbd0
or <sofferor@gmail.com>
2017-05-23 17:14:04 +0300

    poo

--- SearchAlgorithmsLib/GUI/Controls/MazeControl.xaml.cs
+++ SearchAlgorithmsLib/GUI/Controls/MazeControl.xaml.cs
@@ -21,5 +21,96 @@ namespace GUI.Controls {
         public MazeControl() {
             InitializeComponent();
         }
+
+
+
+        public string MazeString {
+            get { return (string)GetValue(MazeStringProperty); }
+            set { SetValue(MazeStringProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MazeString.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MazeStringProperty =
+            DependencyProperty.Register("MazeString", typeof(string), typeof(MazeControl));
+
+
+
+        public int Rows {
+            get { return (int)GetValue(RowsProperty); }
+            set { SetValue(RowsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Rows.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RowsProperty =
+            DependencyProperty.Register("Rows", typeof(int), typeof(MazeControl), new PropertyMetadata(0));
+
+
+
+
+        public int Cols {
+            get { return (int)GetValue(ColsProperty); }
+            set { SetValue(ColsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Cols.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ColsProperty =
+            DependencyProperty.Register("Cols", typeof(int), typeof(MazeControl), new PropertyMetadata(0));
+
+
+
+
+
+        public string InitialPos {
+            get { return (string)GetValue(InitialPosProperty); }
+            set { SetValue(InitialPosProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for InitialPos.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InitialPosProperty =
+            DependencyProperty.Register("InitialPos", typeof(string), typeof(MazeControl));
+
+
+        public string GoalPos {
+            get { return (string)GetValue(GoalPosProperty); }
+            set { SetValue(GoalPosProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for InitialPos.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty GoalPosProperty =
+            DependencyProperty.Register("GoalPos", typeof(string), typeof(MazeControl));
+
+
+
+        public string PlayerImageFile {
+            get { return (string)GetValue(PlayerImageFileProperty); }
+            set { SetValue(PlayerImageFileProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for PlayerImageFile.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlayerImageFileProperty =
+            DependencyProperty.Register("PlayerImageFile", typeof(string), typeof(MazeControl));
+
+
+
+
+        public string ExitImageFile {
+            get { return (string)GetValue(ExitImageFileProperty); }
+            set { SetValue(ExitImageFileProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ExitImageFile.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ExitImageFileProperty =
+            DependencyProperty.Register("ExitImageFile", typeof(string), typeof(MazeControl));
+
+
+
+
+        private void MazeBoardLoaded(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void MazeBoardKeyDown(object sender, KeyEventArgs e) {
+
+        }
     }
 }
