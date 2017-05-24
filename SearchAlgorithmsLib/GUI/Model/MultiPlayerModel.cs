d6857cd0d3daa4892a4fea5bf4cf399b133e6bd9
or <sofferor@gmail.com>
2017-05-25 02:16:21 +0300

    multi

---
+++ SearchAlgorithmsLib/GUI/Model/MultiPlayerModel.cs
@@ -0,0 +1,55 @@
+﻿using MazeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Model {
+    public class MultiPlayerModel : PlayerModel {
+        public event EventHandler<Maze> MazeGenerated;
+        private Position otherPos;
+        private string games;
+
+        public MultiPlayerModel() : base() { }
+
+        public void GenerateMaze(string name, int rows, int cols) {
+            Send("generate " + name + " " + rows.ToString() + " " + cols.ToString());
+            string mazeString = Receive();
+            maze = Maze.FromJSON(mazeString);
+            curPos = maze.InitialPos;
+            OnMazeGenerated(maze);
+        }
+
+        public void Start(string name, int rows, int cols) {
+            Send("start " + name + " " + rows.ToString() + " " + cols.ToString());
+            string mazeString = Receive();
+            maze = Maze.FromJSON(mazeString);
+            curPos = maze.InitialPos;
+            otherPos = curPos;
+            OnMazeGenerated(maze);
+        }
+
+        public void Join(string name) {
+            Send("join " + name);
+            string mazeString = Receive();
+            maze = Maze.FromJSON(mazeString);
+            curPos = maze.InitialPos;
+            otherPos = curPos;
+            OnMazeGenerated(maze);
+        }
+
+        protected virtual void OnMazeGenerated(Maze m) {
+            MazeGenerated?.Invoke(this, m);
+        }
+
+        public string GameAt(int i) {
+            string[] gameList;
+            if (games != null) {
+                gameList = games.Replace("[", "").Replace("]", "").Replace("\n", "").Split(',');
+                return gameList[i];
+            }
+            return null;
+        }
+    }
+}
---
+++ SearchAlgorithmsLib/GUI/View/MultiPlayerView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using GUI.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI.View {
+    /// <summary>
+    /// Interaction logic for MultiPlayerView.xaml
+    /// </summary>
+    public partial class MultiPlayerView : Window {
+
+        public MultiPlayerViewModel vm;
+
+        public MultiPlayerView(MultiPlayerViewModel spvm) {
+            InitializeComponent();
+            vm = spvm;
+            DataContext = vm;
+            MazeControl.DataContext = vm;
+            OtherMazeControl.DataContext = vm;
+            vm.PropertyChanged += delegate(Object sender, PropertyChangedEventArgs e) {
+                if (e.PropertyName == "mazeGenerated") {
+                    MazeControl.DrawMazeBoard();
+                    OtherMazeControl.DrawMazeBoard();
+                }
+            };
+            vm.Initialize(Properties.Settings.Default.ServerIP, Properties.Settings.Default.ServerPort);
+        }
+
+        private void MazeBoardKeyDown(object sender, KeyEventArgs e) {
+        }
+    }
+}
