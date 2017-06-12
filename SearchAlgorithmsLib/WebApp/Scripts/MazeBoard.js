74b08add970f897bf0802649934ded35ad95aef8
Haim Weitman <weithaim@gmail.com>
2017-06-12 18:39:33 +0300

    single player

---
+++ SearchAlgorithmsLib/WebApp/Scripts/MazeBoard.js
@@ -0,0 +1,22 @@
+﻿(function($) {
+    $.fn.mazeBoard = function (mazeData, startRow, startCol,
+        exitRow, exitCol, playerImage, exitImage) {
+        var mazeObj = {
+            mazeData: mazeData,
+            startRow: startRow,
+            startCol: startCol,
+            exitRow: exitRow,
+            exitCol: exitCol,
+            playerImage: playerImage,
+            exitImage: exitImage,
+            isEnabled: true,
+            Draw: function() {
+                
+            },
+            Move: function() {
+                
+            }
+        }
+        return mazeObj;
+    }
+})(jQuery)
