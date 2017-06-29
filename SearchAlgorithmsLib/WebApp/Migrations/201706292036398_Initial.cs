a93e63ab2f37352c7fd655f162687f4e6465c67a
Haim Weitman <weithaim@gmail.com>
2017-06-30 01:14:51 +0300

    db

---
+++ SearchAlgorithmsLib/WebApp/Migrations/201706292036398_Initial.cs
@@ -0,0 +1,30 @@
+namespace WebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        UserName = c.String(nullable: false, maxLength: 128),
+                        Password = c.String(nullable: false),
+                        Email = c.String(),
+                        Wins = c.Int(nullable: false),
+                        Loses = c.Int(nullable: false),
+                        Rank = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.UserName);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
