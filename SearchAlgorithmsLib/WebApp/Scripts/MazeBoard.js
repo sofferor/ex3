dd5c9f0d068c462c9a75a3728539aaefeba4aad1
Haim Weitman <weithaim@gmail.com>
2017-06-29 14:36:02 +0300

    single player

--- SearchAlgorithmsLib/WebApp/Scripts/MazeBoard.js
+++ SearchAlgorithmsLib/WebApp/Scripts/MazeBoard.js
@@ -1,6 +1,12 @@
 ﻿(function($) {
-    $.fn.mazeBoard = function (mazeData, mazeString, startRow, startCol,
-        exitRow, exitCol, playerImage, exitImage) {
+    $.fn.mazeBoard = function(mazeData,
+        mazeString,
+        startRow,
+        startCol,
+        exitRow,
+        exitCol,
+        cols,
+        rows) {
         console.log("in plugin");
         var mazeObj = {
             mazeData: mazeData,
@@ -9,14 +15,103 @@
             startCol: startCol,
             exitRow: exitRow,
             exitCol: exitCol,
-            playerImage: playerImage,
-            exitImage: exitImage,
+            rows: rows,
+            cols: cols,
+            currRow: startRow,
+            currCol: startCol,
+            cellWidth: 0,
+            cellHight: 0,
             isEnabled: true,
             Draw: function() {
-                
+                console.log("in draw");
+                var canvas = document.getElementById("boardCanvas");
+                var context = canvas.getContext("2d");
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                this.cellWidth = canvas.width / cols;
+                this.cellHight = canvas.height / rows;
+                console.log("cellWidth: " + this.cellWidth);
+                console.log("cellHight: " + this.cellHight);
+                var x = 0;
+                var y = 0;
+                for (var i = 0; i < this.mazeString.length; i++) {
+                    if (i % cols == 0 && i != 0) {
+                        x = 0;
+                        y++;
+                    }
+                    if (this.mazeString[i] === "1") {
+                        context.fillRect(this.cellWidth * x, this.cellHight * y, this.cellWidth, this.cellHight);
+                    }
+                    x++;
+                }
+                var p = document.getElementById("startImg");
+                var e = document.getElementById("endImg");
+                console.log(p);
+                console.log(e);
+                context.drawImage(p, startCol * this.cellWidth, startRow * this.cellHight, this.cellWidth, this.cellHight);
+                context.drawImage(e, exitCol * this.cellWidth, exitRow * this.cellHight, this.cellWidth, this.cellHight);
+
             },
-            Move: function() {
+            Move: function (direction) {
+                console.log("in move()");
+                var canvas = document.getElementById("boardCanvas");
+                var context = canvas.getContext("2d");
+                var p = document.getElementById("startImg");
+                //oneDimensionalArray[row * columns + column]
+                if (this.currRow == this.exitRow && this.currCol == this.exitCol) {
+                    return;
+                }
+                switch (direction) {
+                    case "right":
+                        console.log("currCol = " + this.currCol);
+                        console.log("val in maze: " + mazeString[this.currRow * cols + (this.currCol + 1)]);
+                        if (this.currCol + 1 < this.cols && mazeString[this.currRow * cols + (this.currCol + 1)] == 0) {
+                            context.clearRect(this.currCol * this.cellWidth, this.currRow * this.cellHight, this.cellWidth, this.cellHight);
+                            context.drawImage(p, (this.currCol + 1) * this.cellWidth, this.currRow * this.cellHight, this.cellWidth, this.cellHight);
+                            this.currCol = this.currCol + 1;
+                            console.log("newCurrCol = " + this.currCol);
+                        }
+                        break;
+
+                    case "left":
+                        console.log("currCol = " + this.currCol);
+                        console.log("val in maze: " + mazeString[this.currRow * cols + (this.currCol - 1)]);
+                        if (this.currCol - 1 > -1 && mazeString[this.currRow * cols + (this.currCol - 1)] == 0) {
+                            context.clearRect(this.currCol * this.cellWidth, this.currRow * this.cellHight, this.cellWidth, this.cellHight);
+                            context.drawImage(p, (this.currCol - 1) * this.cellWidth, this.currRow * this.cellHight, this.cellWidth, this.cellHight);
+                            this.currCol = this.currCol - 1;
+                            console.log("newCurrCol = " + this.currCol);
+                        }
+                        break;
+
+                    case "up":
+                        console.log("currRow = " + this.currRow);
+                        console.log("val in maze: " + mazeString[(this.currRow - 1) * cols + this.currCol]);
+                        if (this.currRow - 1 > -1 && mazeString[(this.currRow - 1) * cols + this.currCol] == 0) {
+                            context.clearRect(this.currCol * this.cellWidth, this.currRow * this.cellHight, this.cellWidth, this.cellHight);
+                            context.drawImage(p, this.currCol * this.cellWidth, (this.currRow - 1) * this.cellHight, this.cellWidth, this.cellHight);
+                            this.currRow = this.currRow - 1;
+                            console.log("newCurrRow = " + this.currRow);
+                        }
+                        break;
+
+                    case "down":
+                        console.log("currRow = " + this.currRow);
+                        console.log("val in maze: " + mazeString[(this.currRow + 1) * cols + this.currCol]);
+                        if (this.currRow + 1 < this.rows && mazeString[(this.currRow + 1) * cols + this.currCol] == 0) {
+                            context.clearRect(this.currCol * this.cellWidth, this.currRow * this.cellHight, this.cellWidth, this.cellHight);
+                            context.drawImage(p, this.currCol * this.cellWidth, (this.currRow + 1) * this.cellHight, this.cellWidth, this.cellHight);
+                            this.currRow = this.currRow + 1;
+                            console.log("newCurrRow = " + this.currRow);
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
                 
+                if (this.currRow == this.exitRow && this.currCol == this.exitCol) {
+                    setTimeout(function () { alert("Congrats !!! You Won !\nYou can start a New Game."); }, 100);
+                }
             }
         }
         return mazeObj;
--- SearchAlgorithmsLib/WebApp/Scripts/SinglePlayer.js
+++ SearchAlgorithmsLib/WebApp/Scripts/SinglePlayer.js
@@ -1,1 +1,38 @@
-﻿function StatGame() {}
+﻿var maze;
+function StartGame() {
+    console.log("in StartGame()");
+    var url = "/api/SinglePlayer?" +
+        "name=" + $("#mazeName").val() +
+        "&" +
+        "rows=" + $("#mazeRows").val() +
+        "&" +
+        "cols=" + $("#mazeCols").val();
+    console.log(url);
+    $.getJSON(url).done(function (data) {
+        console.log(data);
+        maze = $.fn.mazeBoard(data, data.Maze, data.Start.Row, data.Start.Col, data.End.Row, data.End.Col, data.Cols, data.Rows);
+        console.log(maze);
+        maze.Draw();
+        console.log("after Draw");
+    });
+};
+
+$(this).keydown(function (e) {
+    console.log("in keydown()");
+    switch (e.which) {
+        case 37:
+            maze.Move("left");
+            break;
+        case 38:
+            maze.Move("up");
+            break;
+        case 39:
+            maze.Move("right");
+            break;
+        case 40:
+            maze.Move("down");
+            break;
+        default:
+            break;
+    }
+});
